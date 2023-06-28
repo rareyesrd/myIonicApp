b94c417e1602938003b446418cb636072d643905
rareyesrd <rafaelalexanderr5@gmail.com>
2023-06-28 07:38:57 -0400

    add: login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -21,6 +21,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Login from './pages/Login';
 
 setupIonicReact();
 
@@ -29,7 +30,7 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonRouterOutlet>
         <Route exact path="/home">
-          <Home />
+          <Login />
         </Route>
         <Route exact path="/">
           <Redirect to="/home" />
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,46 @@
 import {
+  IonButton,
+  IonCard,
+  IonCardContent,
   IonContent,
   IonFooter,
   IonHeader,
+  IonInput,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 
 const Login: React.FC = () => {
+  const [isTouched, setIsTouched] = useState(false);
+  const [isValid, setIsValid] = useState<boolean>();
+
+  const validateEmail = (email: string) => {
+    return email.match(
+      /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    );
+  };
+
+  const validate = (ev: Event) => {
+    const value = (ev.target as HTMLInputElement).value;
+
+    setIsValid(undefined);
+
+    if (value === "") return;
+
+    validateEmail(value) !== null ? setIsValid(true) : setIsValid(false);
+  };
+
+  const markTouched = () => {
+    setIsTouched(true);
+  };
+
+  const doLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("doLogin");
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,13 +48,37 @@ const Login: React.FC = () => {
           <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">UI goes here...</IonContent>
+      <IonContent className="ion-padding">
+        <IonCard>
+          <IonCardContent>
+            <form onSubmit={doLogin}>
+              <IonInput
+                className={`${isValid && "ion-valid"} ${
+                  isValid === false && "ion-invalid"
+                } ${isTouched && "ion-touched"}`}
+                type="email"
+                fill="solid"
+                label="Email"
+                labelPlacement="floating"
+                helperText="Enter a valid email"
+                errorText="Invalid email"
+                onIonInput={(event) => validate(event)}
+                onIonBlur={() => markTouched()}
+              ></IonInput>
 
-      <IonFooter>
-        <IonToolbar>Footer</IonToolbar>
-        <IonToolbar>Footer</IonToolbar>
-        <IonToolbar>Footer</IonToolbar>
-      </IonFooter>
+              <IonInput
+                className="ion-margin-top"
+                type="password"
+                fill="solid"
+                label="Password"
+                labelPlacement="floating"
+              ></IonInput>
+
+              <IonButton expand="block">Login</IonButton>
+            </form>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
     </IonPage>
   );
 };
